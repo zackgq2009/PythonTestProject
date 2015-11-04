912e03b67a160319b203a05440071be7a4793b80
qguo <qguo@ilegendsoft.com>
2015-11-04 18:34:07 +0800

    commit all files

---
+++ htmlrenderer.py
@@ -0,0 +1,11 @@
+__author__ = 'johnny'
+
+class HTMLRenderer:
+    def start_paragraph(self):
+        print('<p>')
+    def end_paragraph(self):
+        print('</p>')
+    def sub_emphasis(self, match):
+        return '<em>%s</em>' % match.group(1)
+    def feed(self, data):
+        print(data)
