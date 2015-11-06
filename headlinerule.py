89162a35433f5805a517640bf2bce8156526c2da
qguo <qguo@ilegendsoft.com>
2015-11-06 11:31:54 +0800

    delete the files

--- headlinerule.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'johnny'
-
-class HeadlineRule:
-    def condition(self, block):
-
-    def action(self, block, handler):
--- project001/README.md
+++
@@ -1,2 +0,0 @@
-# PythonTestProject
-####帮助自己学习python，以及写一些python的项目
