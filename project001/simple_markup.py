19f6c59b000673fb7780b6589fd1463e141da922
qguo <qguo@ilegendsoft.com>
2015-11-06 11:28:49 +0800

    add 'project001' package,and move the files in it

---
+++ project001/simple_markup.py
@@ -0,0 +1,22 @@
+__author__ = 'johnny'
+import sys
+import re
+
+from project001.util import *
+
+
+print('<html><head><title>...</title><body>')
+
+title = True
+for block in blocks(sys.stdin):
+    block = re.sub(r'\*(.+?)\*', r'<em>\1</em>', block)
+    if title:
+        print('<h1>')
+        print(block)
+        print('</h1>')
+        title = False
+    else:
+        print('<p>')
+        print(block)
+        print('</p>')
+print('</body></html>')
